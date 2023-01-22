e2a07c64603bd0842354bf03c8e501df4b3db458
_its_not_real_ <lolitsnotreal@protonmail.com>
2023-01-22 13:30:00 -0600

    Updated the user schema, created a graphs page, and created the initial database schema.

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,22 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import WebAuthnClient from '@redwoodjs/auth/webAuthn'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
+import './scaffold.css'
 import './index.css'
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
+      <AuthProvider type="dbAuth" client={WebAuthnClient}>
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </AuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
--- web/src/pages/GraphsPage/GraphsPage.js
+++ web/src/pages/GraphsPage/GraphsPage.js
@@ -6,7 +6,7 @@ const GraphsPage = () => {
   const drawChart = function()  {
     const data = [12, 5, 6, 6, 9, 10];
 
-    const svg = d3.select("body")
+    const svg = d3.select("#SimpleBarChart")
       .append("svg")
       .attr("width", 700)
       .attr("height", 300);
@@ -21,12 +21,13 @@ const GraphsPage = () => {
       .attr("height", (d, i) => d * 10)
       .attr("fill", "green");
   }
+  drawChart()
   return (
     <>
       <MetaTags title="Graphs" description="Graphs page" />
 
       <h1>GraphsPage</h1>
-      <span id={"SimpleLineChart"}></span>
+      <span id={"SimpleBarChart"}></span>
     </>
   )
 }
